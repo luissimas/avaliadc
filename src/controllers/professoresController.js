03a9b2a3d81f84e995cc10cb8e15e7f79ad9530d
Luis Simas <luissimas@protonmail.com>
2021-04-17 20:10:06 +0000

    Finishing professor controllers

--- src/controllers/professoresController.js
+++ src/controllers/professoresController.js
@@ -1,6 +1,22 @@
 const connection = require('../database/connection');
 
 module.exports = {
+  async list(request, response) {
+    const { page = 1 } = request.query;
+
+    const count = await connection('professores').count().first();
+
+    const professores = await connection('professores')
+      .limit(10)
+      .offset((page - 1) * 5)
+      .select();
+
+    // Retornando o total de professores cadastrados pelo header da resposta
+    response.header('X-Total-Count', count['count(*)']);
+
+    return response.json(professores);
+  },
+
   async create(request, response) {
     const { nome, qualificacao } = request.body;
 
@@ -13,7 +29,7 @@ module.exports = {
   },
 
   async delete(request, response) {
-    const { id } = request.params;
+    const { id } = request.body;
 
     // O método delete retorna o número de linhas afetadas, podemos usar isso para verificar se a operação foi bem sucedida
     const rows = await connection('professores').where('id', id).delete();
@@ -28,10 +44,11 @@ module.exports = {
   },
 
   async update(request, response) {
-    const { id } = request.params;
+    const { id, nome, qualificacao } = request.body;
 
     const rows = await connection('professores').where('id', id).update({
-
+      nome: nome,
+      qualificacao: qualificacao,
     });
 
     if (rows === 0) {
--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,9 @@ const avaliacoesController = require('./controllers/avaliacoesController')
 
 const routes = express.Router();
 
-routes.get('/professores', professoresController.create)
-routes.delete('/professores/delete/:id', professoresController.delete)
-routes.post('/professores/update/:id', professoresController.update)
+routes.get('/professores/list', professoresController.list)
+routes.get('/professores/create', professoresController.create)
+routes.delete('/professores/delete', professoresController.delete)
+routes.post('/professores/update', professoresController.update)
 
 module.exports = routes
