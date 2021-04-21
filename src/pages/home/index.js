77fe6b9b0c8df702960f9ae5340cd893952bf55f
Luis Simas <luissimas@protonmail.com>
2021-04-20 21:11:43 -0300

    Base for home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,20 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import api from '../../services/api'
+import api from "../../services/api";
 
-import './style.css'
+import "./style.css";
 
-export default function Home(){
-  const [professores, setProfessores] = useState([])
+export default function Home() {
+  const [professores, setProfessores] = useState([]);
 
   // useEffect que carrega os dados da api apenas quando o app é carregado pela primeira vez
   useEffect(() => {
-  }, [])
+    api.get("/professores").then((response) => {
+      setProfessores(response.data);
+    });
+  }, []);
 
   return (
     <div className="home-container">
-      <header>
-      </header>
+      <header></header>
+
+      <h1>Professores cadastrados</h1>
+
+      <ul>
+        {professores.map((professor) => {
+          return (
+            <Link className="card-button" to="/professor">
+              <li key={professor.id}>
+                <strong>{professor.nome}</strong>
+                <p>{professor.qualificacao}</p>
+                <p>{professor.avaliacoes ? 'Média: ' + professor.media:''}</p>
+                <p>{professor.avaliacoes ? professor.avaliacoes:'Sem avaliações' }</p>
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
     </div>
   );
 }
