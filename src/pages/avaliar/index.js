b93c3e9c2a46cdacc8c0884eafa286ce74178f71
Luis Simas <luissimas@protonmail.com>
2021-04-21 09:51:21 -0300

    Starting to work on avaliar page

---
+++ src/pages/avaliar/index.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react"
+import { useHistory } from "react-router-dom";
+import StarRatings from "react-star-ratings";
+
+import api from "../../services/api";
+
+import "./style.css";
+
+export default function Avaliar(){
+  return(
+    <div className="avaliar-container">
+      <header>
+      </header>
+    </div>
+  )
+}
