a2f782d2bf52c2b0ea9a8c8ae05dc3fa13f6f34c
Luis Simas <luissimas@protonmail.com>
2021-04-22 12:39:49 -0300

    Fixing merge errors

--- backend/src/database/seeds/002_avaliacoes.js
+++ backend/src/database/seeds/002_avaliacoes.js
@@ -3,8 +3,50 @@ exports.seed = function (knex) {
   return knex('avaliacoes')
     .del()
     .then(function () {
+      let i;
+      let avaliacoesArray = [];
+
+      for (i = 0; i < 20; i++) {
+        avaliacoesArray.push({
+          curso: 'BCC',
+          ano_ingresso: 2020,
+          comentario: 'Acho isso isso e aquilo',
+          avaliacao_conhecimento: 4,
+          avaliacao_didatica: 3,
+          avaliacao_tirar_duvidas: 2,
+          avaliacao_dialogo: 5,
+          avaliacao_metodo_avaliativo: 2,
+          avaliacao_conteudo_cobrado: 4,
+          avaliacao_correcao: 2,
+          avaliacao_materiais: 4,
+          avaliacao_cuidado_ofensivo: 2,
+          cobra_presenca: true,
+          professor_id: 3,
+        });
+      }
+
+      for (i = 0; i < 6; i++) {
+        avaliacoesArray.push({
+          curso: 'BCC',
+          ano_ingresso: 2020,
+          comentario: 'Acho isso isso e aquilo',
+          avaliacao_conhecimento: 4,
+          avaliacao_didatica: 3,
+          avaliacao_tirar_duvidas: 2,
+          avaliacao_dialogo: 5,
+          avaliacao_metodo_avaliativo: 2,
+          avaliacao_conteudo_cobrado: 4,
+          avaliacao_correcao: 2,
+          avaliacao_materiais: 4,
+          avaliacao_cuidado_ofensivo: 2,
+          cobra_presenca: true,
+          professor_id: 3,
+        });
+      }
+
       // Inserts seed entries
       return knex('avaliacoes').insert([
+        ...avaliacoesArray,
         {
           curso: 'BCC',
           ano_ingresso: 2020,
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-
-// Controllers
-const professoresController = require('./controllers/professoresController');
-const avaliacoesController = require('./controllers/avaliacoesController');
-
-// Validação
-const professoresValidator = require('./middlewares/validation/professoresValidator');
-const avaliacoesValidator = require('./middlewares/validation/avaliacoesValidator');
-
-const routes = express.Router();
-
-// Professores
-routes.get(
-  '/professores',
-  professoresValidator.list,
-  professoresController.list
-);
-
-routes.get(
-  '/professores/:id',
-  professoresValidator.listById,
-  professoresController.listById
-);
-
-routes.post(
-  '/professores',
-  professoresValidator.create,
-  professoresController.create
-);
-
-routes.put(
-  '/professores/:id',
-  professoresValidator.update,
-  professoresController.update
-);
-
-routes.delete(
-  '/professores/:id',
-  professoresValidator.delete,
-  professoresController.delete
-);
-
-// Avaliações
-routes.get(
-  '/avaliacoes/:professor_id',
-  avaliacoesValidator.list,
-  avaliacoesController.list
-);
-
-routes.post(
-  '/avaliacoes',
-  avaliacoesValidator.create,
-  avaliacoesController.create
-);
-
-routes.put(
-  '/avaliacoes/:id',
-  avaliacoesValidator.update,
-  avaliacoesController.update
-);
-
-routes.delete(
-  '/avaliacoes/:id',
-  avaliacoesValidator.delete,
-  avaliacoesController.delete
-);
-
-module.exports = routes;
-=======
 import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
@@ -89,4 +18,3 @@ export default function Routes(){
     </Router>
   );
 }
->>>>>>> frontend/master
