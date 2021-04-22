30e95fcc7a835a5c7a3982753fdf5928f9e6ae18
Luis Simas <luissimas@protonmail.com>
2021-04-22 10:57:29 -0300

    Adding infinite scroll pagination on home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import api from "../../services/api";
 
@@ -7,16 +8,37 @@ import "./style.css";
 
 export default function Home() {
   const [professores, setProfessores] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalProfessores, setTotalProfessores] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   const history = useHistory();
 
-  // useEffect que carrega os dados da api apenas quando o app é carregado pela primeira vez
-  useEffect(() => {
-    async function getProfessores() {
-      const response = await api.get("/professores");
-      setProfessores(response.data);
+  async function getProfessores() {
+    // Caso os dados já estejam sendo carregados
+    if (loading) {
+      return;
     }
 
+    // Caso já tenham sido carregados todos os professores
+    if (totalProfessores > 0 && professores.length === totalProfessores) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get("/professores", {
+      params: { page: page },
+    });
+
+    // Incluindo os novos professores carregados pela api
+    setProfessores([...professores, ...response.data]);
+    setTotalProfessores(response.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
+  }
+
+  useEffect(() => {
     getProfessores();
   }, []);
 
@@ -37,24 +59,32 @@ export default function Home() {
       <h1>Professores cadastrados</h1>
 
       <ul>
-        {professores.map((professor) => {
-          return (
-            <button
-              className="card-button"
-              onClick={() => navigateToProfessors(professor)}
-            >
-              <li key={professor.id}>
-                <strong>{professor.nome}</strong>
-                <p>{professor.qualificacao}</p>
-                <p>
-                  {professor.avaliacoesCount
-                    ? professor.avaliacoesCount
-                    : "Sem avaliações"}
-                </p>
-              </li>
-            </button>
-          );
-        })}
+        <InfiniteScroll
+          dataLength={professores.length}
+          next={getProfessores}
+          hasMore={professores.length < totalProfessores}
+          loader={<h4>Loading...</h4>}
+    className="list-scroll"
+        >
+          {professores.map((professor) => {
+            return (
+              <button
+                className="card-button"
+                onClick={() => navigateToProfessors(professor)}
+              >
+                <li key={professor.id}>
+                  <strong>{professor.nome}</strong>
+                  <p>{professor.qualificacao}</p>
+                  <p>
+                    {professor.avaliacoesCount
+                      ? professor.avaliacoesCount
+                      : "Sem avaliações"}
+                  </p>
+                </li>
+              </button>
+            );
+          })}
+        </InfiniteScroll>
       </ul>
     </div>
   );
